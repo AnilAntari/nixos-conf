2342a59008fc5a5354fb7888f1ad5dffe432f7e4
AnilAntari <retro201272@gmail.com>
2023-12-31 01:14:12 +0300

    add ru_RU.UTF-8

--- configuration.nix
+++ configuration.nix
@@ -26,7 +26,7 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "ru_RU.UTF-8";
   console = {
     font = "Lat2-Terminus16";
     keyMap = "us";
@@ -145,6 +145,7 @@
     htop
     ncdu
     qbittorrent
+    libreoffice-still
     # sway
     brightnessctl
     alacritty
