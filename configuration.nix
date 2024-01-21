7b14e1c78ee70cb5c14c48a0c360affcebafb2b8
AnilAntari <retro201272@gmail.com>
2024-01-21 09:27:51 +0300

    add spice

--- configuration.nix
+++ configuration.nix
@@ -177,7 +177,9 @@
     gimp
     shellcheck
     chromium
-        
+    spice
+    spice-gtk
+              
     # sway
     brightnessctl
     alacritty
@@ -194,6 +196,7 @@
     
   ];
 
+  virtualisation.spiceUSBRedirection.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
